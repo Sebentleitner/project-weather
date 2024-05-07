1e09ee9061377ca0c27985478d18cb6acb048f15
sebentleitner <sentleitner@schueler.hakzell.at>
2024-05-07 11:31:58 +0200

    used day

--- index.php
+++ index.php
@@ -7,11 +7,9 @@ require 'vendor/autoload.php';
 
 $history = new History();
 $history->addDay(new Day("2024-05-04", "sonnig"));
-$history->addDay(new Day("2024-05-05", "schnee"));
-$history->addDay(new Day("2024-05-06", "Regnerisch"));
 
-
-
+$tag2 = new Day('2024-04-29',"schnee");
+$history->addDay($tag2);
 
 $date = $_GET['date'];
 $weather = $history->getWeatherDate($date);
